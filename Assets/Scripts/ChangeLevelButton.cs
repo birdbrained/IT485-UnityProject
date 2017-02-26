918c88df4ac4ab96d840daa12b31aab7090b486b
Matthew Kataryniak <Matthew Kataryniak>
2017-02-26 16:56:14 -0500

    Started to create curved UI elements, got a basic template working

---
+++ Assets/Scripts/ChangeLevelButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayGameButton : MonoBehaviour 
+{
+    public void ChangeLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/Scripts/PlayGameButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeLevelButton : MonoBehaviour 
+{
+    public void ChangeLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
