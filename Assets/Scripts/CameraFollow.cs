26c013b75a8a877ca2c7987c4cdfea748ff4abee
Matthew Kataryniak <Matthew Kataryniak>
2017-02-28 16:27:54 -0500

    UI updates: looking down shows recenter UI in Level1. Needs to be added to other scenes, and need to lower scenes as well.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject camera;
+    [SerializeField]
+    private GameObject player;
+    private float rotationY;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () 
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        rotationY = camera.transform.localEulerAngles.y;
+        transform.localEulerAngles = new Vector3(90, rotationY, 0);
+
+        if (camera.transform.localEulerAngles.x > 40.0f && !animator.GetBool("visible"))
+        {
+            animator.SetBool("visible", true);
+        }
+
+        if (camera.transform.localEulerAngles.x < 40.0f && animator.GetBool("visible"))
+        {
+            animator.SetBool("visible", false);
+        }
+    }
+}
