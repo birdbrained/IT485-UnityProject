d36daf7e64c454a4e0c0124382cc422b4ea078b4
mwk9 <mwk9@njit.edu>
2017-03-21 12:48:24 -0400

    Added more animations, as well as Level2. This level has the enemies animated to make them move around the screen

---
+++ Assets/Scripts/AnimateOnGaze.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateOnGaze : MonoBehaviour 
+{
+
+    public void GazeAnimation(bool gazed)
+    {
+        gameObject.GetComponent<Animator>().SetBool("gazedAt", gazed);
+    }
+
+}
