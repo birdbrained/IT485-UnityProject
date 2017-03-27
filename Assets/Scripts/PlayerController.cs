a65ccab37fffb6a9aa907031fddfc2e1fa75806f
mwk9 <mwk9@njit.edu>
2017-03-27 15:29:45 -0400

    Players now are immortal for a short time after taking damage, and added a player hurt sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,18 @@ using System.Collections;
 
 public class PlayerController : Character
 {
+    private AudioSource hurtSound;
+    private bool immortal = false;
+    [SerializeField]
+    private float immortalTime;
+    [SerializeField]
+    private GameObject juicebox;
 
 	// Use this for initialization
 	void Start () 
     {
         base.Start();
+        hurtSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -29,13 +36,47 @@ public class PlayerController : Character
         base.OnTriggerEnter(other);
     }
 
+    private IEnumerator IndicateImmortality()
+    {
+        while (immortal)
+        {
+            //juicebox.GetComponent<MeshRenderer>().enabled = false;
+            foreach (Transform child in juicebox.transform)
+            {
+                if (child.name != "ShootPos")
+                    child.GetComponent<MeshRenderer>().enabled = false;
+            }
+            //juicebox.SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+            //juicebox.GetComponent<MeshRenderer>().enabled = true;
+            foreach (Transform child in juicebox.transform)
+            {
+                if (child.name != "ShootPos")
+                    child.GetComponent<MeshRenderer>().enabled = true;
+            }
+            //juicebox.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
     public override IEnumerator TakeDamage()
     {
-        health -= 10;
-        if (IsDead)
+        if (!immortal)
         {
-            Death();
-            yield return null;
+            health -= 10;
+            hurtSound.Play();
+            if (!IsDead)
+            {
+                immortal = true;
+                StartCoroutine(IndicateImmortality());
+                yield return new WaitForSeconds(immortalTime);
+                immortal = false;
+            }
+            else
+            {
+                Death();
+                yield return null;
+            }
         }
     }
 
