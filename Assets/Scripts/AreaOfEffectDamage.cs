0cfb7611ab3ca4d5b16e9b8f5a98a39504c8c4f1
mwk9 <mwk9@njit.edu>
2017-03-28 18:33:42 -0400

    Couple touch-ups, sounds and animations

--- Assets/Scripts/AreaOfEffectDamage.cs
+++ Assets/Scripts/AreaOfEffectDamage.cs
@@ -30,6 +30,7 @@ public class AreaOfEffectDamage : MonoBehaviour
             {
                 enemy.StartCoroutine(enemy.TakeDamage(damage));
                 enemy.GetComponent<Rigidbody>().velocity = new Vector3(0f, 5f, 0f);
+                enemy.transform.rotation = Quaternion.Euler(enemy.transform.rotation.x + Random.Range(-15f, 15f), enemy.transform.rotation.y, enemy.transform.rotation.z);
                 Instantiate(damageObj, enemy.transform.position, enemy.transform.rotation);
             }
         }
