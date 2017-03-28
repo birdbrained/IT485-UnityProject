6ef27542514d837f0bb005c5c5f97860d4a13bc3
mwk9 <mwk9@njit.edu>
2017-03-28 17:00:39 -0400

    Did more work on the Grenade Launcher. Should be working good now! Also explosionssss

---
+++ Assets/Scripts/AreaOfEffectDamage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaOfEffectDamage : MonoBehaviour 
+{
+    public float explosionRadius;
+    public int explosionDamage;
+    public GameObject damageObj;
+
+    // Use this for initialization
+    void Start () 
+    {
+        AreaDamageEnemies(gameObject.transform.position, explosionRadius, explosionDamage);
+    }
+	
+    // Update is called once per frame
+    void Update () 
+    {
+        
+    }
+
+    void AreaDamageEnemies(Vector3 location, float radius, int damage)
+    {
+        Collider[] objectsInRange = Physics.OverlapSphere(location, radius);
+        foreach (Collider c in objectsInRange)
+        {
+            Enemy enemy = c.GetComponent<Enemy>();
+            
+            if (enemy != null)
+            {
+                enemy.StartCoroutine(enemy.TakeDamage(damage));
+                enemy.GetComponent<Rigidbody>().velocity = new Vector3(0f, 5f, 0f);
+                Instantiate(damageObj, enemy.transform.position, enemy.transform.rotation);
+            }
+        }
+    }
+}
