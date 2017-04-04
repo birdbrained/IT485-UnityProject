be50a45c8ecadc150643cdf8fb2815a2ca3d43dc
mwk9 <mwk9@njit.edu>
2017-04-03 20:05:14 -0400

    Donut progress bar now attached to player instead of enemies. Need to fix enemies in all scenes.

---
+++ Assets/Scripts/FreezeChildRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeChildRotation : MonoBehaviour 
+{
+    private Quaternion myRotation;
+
+    void Awake()
+    {
+        myRotation = gameObject.transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        gameObject.transform.rotation = myRotation;
+    }
+}
