5c8f381d50b66088815aa52d93336701989b6015
mwk9 <mwk9@njit.edu>
2017-04-04 19:32:10 -0400

    More Tut level more

--- Assets/Scripts/ButtonScripts/EnableObjOnDeath.cs
+++ Assets/Scripts/ButtonScripts/EnableObjOnDeath.cs
@@ -41,4 +41,9 @@ public class EnableObjOnDeath : MonoBehaviour
             }
         }
     }
+
+    public void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
 }
---
+++ Assets/Scripts/GuysScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuysScript : MonoBehaviour 
+{
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeSprite(Sprite lemonLime)
+    {
+        sr.sprite = lemonLime;
+    }
+
+}
