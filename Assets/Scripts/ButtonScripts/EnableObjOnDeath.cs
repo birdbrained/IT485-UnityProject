d069c252138ef2dfdf95386cf242970602f4fb64
mwk9 <mwk9@njit.edu>
2017-04-04 16:44:32 -0400

    Started to add a Tutorial level. Also imported TextMeshPro free, and a material from Substance Share

---
+++ Assets/Scripts/ButtonScripts/EnableObjOnDeath.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableObjOnDeath : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject[] objects;
+    //[SerializeField]
+    //private Vector3 spawnPos;
+    [SerializeField]
+    private GameObject[] disabledObjects;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDisable()
+    {
+        OnDestroy();
+    }
+
+    void OnDestroy()
+    {
+        foreach (GameObject obj in objects)
+        {
+            if (obj != null)
+                obj.SetActive(true);
+        }
+        if (disabledObjects.Length > 0)
+        {
+            foreach (GameObject obj in disabledObjects)
+            {
+                if (obj != null)
+                    obj.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ButtonScripts/TutorialButtons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class TutorialButtons : MonoBehaviour 
+{
+    private TextMeshProUGUI myText;
+
+    void Start ()
+    {
+        myText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void ProgressText(string s)
+    {
+        myText.SetText(s);
+    }
+
+    public void DisableSelf()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void EnableGameObject(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+}
