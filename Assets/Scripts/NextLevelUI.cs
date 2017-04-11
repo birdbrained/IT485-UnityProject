cb0970d9e53d5cf8341c97ff752a8a6c4e03a7f2
mwk9 <mwk9@njit.edu>
2017-04-11 17:55:57 -0400

    Added the MeshBaker, and started to apply it to Level1

--- Assets/Scripts/NextLevelUI.cs
+++ Assets/Scripts/NextLevelUI.cs
@@ -6,11 +6,13 @@ public class NextLevelUI : MonoBehaviour
     [SerializeField]
     private GameObject camera;
     private float rotationY;
+    private AudioSource audio;
 
     // Use this for initialization
-    void Start () 
+    void Awake () 
     {
-        
+        Debug.Log("Awake");
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,5 +20,15 @@ public class NextLevelUI : MonoBehaviour
     {
         rotationY = camera.transform.localEulerAngles.y;
         transform.localEulerAngles = new Vector3(90, rotationY, 0);
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("Enabled UI");
+        if (audio != null)
+        {
+            Debug.Log("Playing Audio");
+            audio.Play();
+        }
     }
 }
